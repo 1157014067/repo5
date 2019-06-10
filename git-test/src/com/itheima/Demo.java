70c6db183c76a350e36ffba64c1bf5632d511512
cc <cc@.com>
2019-06-10 20:11:31 +0800

    提交

---
+++ git-test/src/com/itheima/Demo.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("sfsef");
+        System.out.println("sfsef");
+        System.out.println("sfsef");
+    }
+}
